8e302c9061b495f69fa1adb4c3f9be988a65120d
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-04 18:39:28 -0500

    corrections into scss

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,13 @@ Route::get('/comics', function () {
 })->name('comics');
 
 
+// La route utilizza il metodo get di Laravel per indicare che sarà attiva solo per le richieste HTTP di tipo GET. Il primo parametro della route è l'URL che attiverà questa route, ovvero /comics-detail/{id}. L'URL contiene una variabile di nome id, che sarà passata come argomento alla funzione di callback della route.
+
+// La funzione di callback viene eseguita quando viene effettuata una richiesta all'URL specificato. In questo caso, la funzione esegue alcune operazioni su un array di oggetti (config('db.cards_books')) per filtrare gli oggetti con l'ID specificato nella richiesta. Una volta ottenuto l'oggetto desiderato, viene passato alla vista (template HTML) di nome comics_detail attraverso l'istruzione return view('comics_detail', compact('comic'));. L'istruzione compact crea un array associativo a partire dalle variabili presenti nell'ambiente corrente, in modo che possano essere passate alla vista e utilizzate per generare il contenuto della pagina.
+
+// L'istruzione ->name('comics_detail') assegna un nome alla route, che può essere utilizzato per generare URL dinamicamente nel codice.
+
+
 Route::get('/comics-detail/{id}', function($id) {
     $comics_get = array_filter(config('db.cards_books'), fn ($card) => $card['id'] == $id);
     //dd($comics_get);
