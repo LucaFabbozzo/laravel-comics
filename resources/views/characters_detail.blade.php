426135173acdb5cba468302a39d6921e85d2db38
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-07 10:35:45 -0500

    [add] comics detals page

--- resources/views/characters_detail.blade.php
+++ resources/views/characters_detail.blade.php
@@ -3,12 +3,15 @@
 @section('content')
     <div class="container">
         <div class="detail-box">
-
+            <h2>{{$character['text']}}</h2>
+            <a href="{{route('characters')}}"><img src="{{$character['img']}}" alt="{{$character['text']}}"></a>
         </div>
     </div>
     <div class="container">
         <div class="house">
-            
+             <div class="house">
+                <a href="{{ route('home')}}"><i class="fa-solid fa-house-chimney"></i></a>
+            </div>
         </div>
     </div>
 @endsection
